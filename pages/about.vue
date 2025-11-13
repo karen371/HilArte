f3193692735d073054671b54e40dddb7be7be520
karen371 <karengodoy325@gmail.com>
2025-11-13 15:59:22 -0300

    feat: probando nuxt

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,6 @@
-
 <template>
   <div>
     <h2>Acerca de este proyecto</h2>
     <NuxtLink to="/">Volver al inicio</NuxtLink>
   </div>
 </template>
-
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>¡Hola Karen! 🌸</h1>
+    <p>Bienvenida a tu primer proyecto con Nuxt 💚</p>
+    <NuxtLink to="/about">Ir a la página About</NuxtLink>
+  </div>
+</template>
