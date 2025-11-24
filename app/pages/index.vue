4e4b1752099346255abf168065291e6edae6bbd3
karen371 <karengodoy325@gmail.com>
2025-11-24 19:59:10 -0300

    prueba e instalacion de tailwindcss

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>¡Hola Karen! 🌸</h1>
+    <h1  class="bg-red-500 w-20 h-20">¡Hola Karen! 🌸</h1>
     <p>Bienvenida a tu primer proyecto con Nuxt 💚</p>
     <NuxtLink to="/about">Ir a la página About</NuxtLink>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/fonts']
+  modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxtjs/tailwindcss']
 })
